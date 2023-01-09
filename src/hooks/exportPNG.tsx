379eee1eb217468dc3f2b3ce0a9cbf69e749e348
AttePerttola <atte.perttola@gmail.com>
2023-01-09 23:40:58 +0200

    export png init

--- src/hooks/exportPNG.tsx
+++ src/hooks/exportPNG.tsx
@@ -1,3 +1,15 @@
-export const paska = () => {
-  return 'paska';
+import domtoimage from 'dom-to-image-more';
+import format from 'date-fns/format';
+
+const getFileName = (fileType: string) => {
+  `${format(new Date(), "'Seats-'HH-mm-ss")}.${fileType}`;
+};
+
+export const exportPNG = (node: HTMLElement) => {
+  domtoimage.toPng(node).then(function (dataUrl: string) {
+    const link = document.createElement('a');
+    link.download = `${getFileName('png')}`;
+    link.href = dataUrl;
+    link.click();
+  });
 };
--- src/pages/editor/Editor.tsx
+++ src/pages/editor/Editor.tsx
@@ -12,6 +12,7 @@ export const Editor = () => {
         <Canvas />
         <AddTableButton />
       </CanvasWindow>
+
       <Sidebar />
     </Grid>
   );
