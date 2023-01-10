cfe99301660fefa353c9a505324d39561a22c96d
AttePerttola <atte.perttola@gmail.com>
2023-01-10 19:41:15 +0200

    png file date and background

--- src/hooks/exportPNG.tsx
+++ src/hooks/exportPNG.tsx
@@ -2,13 +2,13 @@ import domtoimage from 'dom-to-image-more';
 import format from 'date-fns/format';
 
 const getFileName = (fileType: string) => {
-  `${format(new Date(), "'Seats-'HH-mm-ss")}.${fileType}`;
+  return `${format(new Date(), "'Seats-'dd.MM.yyyy")}.${fileType}`;
 };
 
 export const exportPNG = (node: HTMLElement) => {
   domtoimage.toPng(node).then(function (dataUrl: string) {
     const link = document.createElement('a');
-    link.download = `${getFileName('png')}`;
+    link.download = getFileName('png');
     link.href = dataUrl;
     link.click();
   });
