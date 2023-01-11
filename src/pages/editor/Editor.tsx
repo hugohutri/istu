1100e9cc426ba1df6135a4355a4978da8137d542
Mvke <lahteenmaki.make@gmail.com>
2023-01-11 17:02:53 +0200

    feat: color information

--- src/pages/editor/Editor.tsx
+++ src/pages/editor/Editor.tsx
@@ -1,40 +1,41 @@
-import styled from 'styled-components'
-import { AddTableButton } from '../../modals/AddTableModal'
-import { Sidebar } from './components/sidebar/Sidebar'
-import { Canvas } from './components/Canvas'
+import styled from 'styled-components';
+import { Sidebar } from './components/sidebar/Sidebar';
+import { Canvas } from './components/Canvas';
 
-import 'react-indiana-drag-scroll/dist/style.css'
+import 'react-indiana-drag-scroll/dist/style.css';
+import { ColorInfo } from './components/ColorInfo';
 
 export const Editor = () => {
-    return (
-        <Grid>
-            <CanvasWindow>
-                <Canvas />
-            </CanvasWindow>
+  return (
+    <Grid>
+      <CanvasWindow>
+        <Canvas />
+        <ColorInfo />
+      </CanvasWindow>
 
-            <Sidebar />
-        </Grid>
-    )
-}
+      <Sidebar />
+    </Grid>
+  );
+};
 
 export const Grid = styled.div`
-    flex: 1;
-    flex-grow: 1;
-    display: grid;
-    overflow: hidden;
+  flex: 1;
+  flex-grow: 1;
+  display: grid;
+  overflow: hidden;
 
-    // This defines the width of the sidebar
-    grid-template-columns: 1fr 300px;
-`
+  // This defines the width of the sidebar
+  grid-template-columns: 1fr 300px;
+`;
 
 /**
  * This element stays at the same size, but the content inside it can be scrolled
  * Fixed ui elements like canvas actions should be placed inside this element
  */
 const CanvasWindow = styled.div`
-    flex: 1;
-    display: flex;
-    overflow: hidden;
-    position: relative;
-    scale: 1;
-`
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+  position: relative;
+  scale: 1;
+`;
