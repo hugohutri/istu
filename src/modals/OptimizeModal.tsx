8164c960ba7c51bfce259a51421be05846dd955c
hugohutri <55588133+hugohutri@users.noreply.github.com>
2023-01-16 10:13:31 +0200

    wip: optimize mvp

--- src/modals/OptimizeModal.tsx
+++ src/modals/OptimizeModal.tsx
@@ -4,15 +4,26 @@ import { Button } from '../components/uikit/Button';
 import { Modal } from '../components/uikit/Modal';
 import { Guest } from '../hooks/types';
 import { useGuests } from '../hooks/useGuests';
-import { useGetSeatsWithLocation } from '../hooks/useSeatWithLocation';
+import {
+  SeatWithLocation,
+  useGetSeatsWithLocation,
+} from '../hooks/useSeatWithLocation';
 
 export const OptimizeModal = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const { iterations, isOptimizing, optimize, stop } = useOptimizer();
+  const { iterations, isOptimizing, optimize, stop, scores } = useOptimizer();
 
   const onClose = () => {
     stop();
     setIsOpen(false);
+  };
+
+  // const scoresString = scores.map((s) => s.toFixed(0)).join(', ');
+
+  const getUIScore = () => {
+    const bestScore = Math.max(...scores, 0);
+    // return bestScore.toFixed(2) * 100;
+    return (bestScore * 100).toFixed(0) + '%';
   };
 
   return (
@@ -22,81 +33,65 @@ export const OptimizeModal = () => {
       </Button>
       <Modal title="Optimizer" isOpen={isOpen} onClose={onClose}>
         <Body>Optimizer iterations: {iterations}</Body>
+        {/* <Body>Optimizer scores: {scoresString}</Body> */}
+        <Body>Optimizer best score: {getUIScore()}</Body>
         <Body>Optimizer is running: {isOptimizing ? 'Yes' : 'No'}</Body>
-        <Button onClick={() => optimize(10)}>Optimize</Button>
+        <Button onClick={() => optimize(30000)}>Optimize</Button>
       </Modal>
     </>
   );
 };
 
+function randomOrder<T>(array: T[]) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
 function useOptimizer() {
   const getSeatsWithLocation = useGetSeatsWithLocation();
-  const guests = useGuests((s) => s.guests);
-  const assignSeat = useGuests((s) => s.assignSeat);
+  const storedGuests = useGuests((s) => s.guests);
+  //   const assignSeat = useGuests((s) => s.assignSeat);
   const setGuests = useGuests((s) => s.setGuests);
   const [iterations, setIterations] = useState(0);
   const [isOptimizing, setIsOptimizing] = useState(false);
-
-  const runIteration = async () => {
-    let seats = getSeatsWithLocation();
-    // console.table(seats);
-    // console.table(guests);
-
-    setGuests(removeOldSeats(guests));
-    await wait(100);
-
-    const companionPairs = getGuestsInPairs(guests);
-
-    for (const [guest, companion] of companionPairs) {
-      for (const seat of seats) {
-        const companionSeat = seats.find((s) => s.companionSeatId === seat.id);
-        if (!seat || !companionSeat) break;
-
-        assignSeat(seat, guest);
-        assignSeat(companionSeat, companion);
-
-        // remove the seats from the list
-        seats = seats.filter(
-          ({ id }) => id !== seat.id && id !== companionSeat.id
-        );
-
-        await wait(100);
-        break;
-      }
-    }
-
-    const guestsWithoutCompanion = guests.filter((guest) => !guest.avecName);
-    for (const guest of guestsWithoutCompanion) {
-      if (guest.seat) continue;
-      const seat = seats.at(0);
-      if (!seat) break;
-
-      assignSeat(seat, guest);
-      seats = seats.filter(({ id }) => id !== seat.id);
-      await wait(100);
-    }
-  };
+  const [scores, setScores] = useState<number[]>([]);
+  // let isStopped = false;
+  // const [bestGuests, setBestGuests] = useState<Guest[]>([]);
 
   const optimize = async (iterations: number) => {
     setIsOptimizing(true);
     setIterations(0);
-    await wait(100);
+    setScores([]);
+    const seats = getSeatsWithLocation();
+    await wait(10);
+    setGuests(removeOldSeats(storedGuests));
+    let bestScore = 0;
 
     for (let i = 0; i < iterations; i++) {
-      //   if (!isOptimizing) {
-      //     break;
-      //   }
-      await runIteration();
+      // if (isStopped) return;
+      if (i % 500 === 0) await wait(1);
+      // await wait(1);
+
+      const { guests, score } = await runIteration(storedGuests, seats);
+      // console.log('score', score);
+      // setGuests(guests);
+      setScores((s) => [...s, score]);
       setIterations((i) => i + 1);
+
+      if (score > bestScore) {
+        // setBestGuests(guests);
+        console.log('New best score', score);
+        bestScore = score;
+        setGuests(guests);
+      }
     }
     setIsOptimizing(false);
   };
 
   const stop = () => {
-    setIsOptimizing(false);
+    setIterations(0);
   };
 
-  return { optimize, isOptimizing, iterations, stop };
+  return { optimize, isOptimizing, iterations, stop, scores };
 }
 
 const removeOldSeats = (guests: Guest[]) => {
@@ -128,23 +123,134 @@ function getGuestsInPairs(guests: Guest[]) {
   return pairs;
 }
 
-// function calculateArrangementScore(guests: Guest[], seats: Seats) {
-//   const guestsWithCompanion = guests.filter((guest) => guest.avecName);
+async function runIteration(storedGuests: Guest[], seats: SeatWithLocation[]) {
+  const copiedGuests = JSON.parse(JSON.stringify(storedGuests)) as Guest[];
+  const guests = randomOrder(copiedGuests);
 
-//   let score = 0;
-//   for (const guest of guestsWithCompanion) {
-//     const companion = guests.find((g) => g.name === guest.avecName);
-//     if (!companion) continue;
+  const guestToSeat = new Map<Guest, SeatWithLocation>();
 
-//     if (guest.seat && companion.seat) {
-//       const distance = getDistanceBetweenSeats(guest.seat, companion.seat);
-//       score += distance;
-//     }
-//   }
+  const companionPairs = getGuestsInPairs(guests);
 
-//   return score;
-// }
+  for (const [guest, companion] of companionPairs) {
+    for (const seat of seats) {
+      const companionSeat = seats.find((s) => s.companionSeatId === seat.id);
+      if (!seat || !companionSeat) continue;
 
-// function getDistanceBetweenSeats(seat: Seat, seat1: Seat) {
-//   return Math.abs(seat.x - seat1.x) + Math.abs(seat.y - seat1.y);
-// }
+      guest.seat = seat;
+      companion.seat = companionSeat;
+      guestToSeat.set(guest, seat);
+      guestToSeat.set(companion, companionSeat);
+
+      // remove the seats from the list
+      seats = seats.filter(
+        ({ id }) => id !== seat.id && id !== companionSeat.id
+      );
+
+      break;
+    }
+  }
+
+  const guestsWithoutCompanion = guests.filter((guest) => !guest.avecName);
+  for (const guest of guestsWithoutCompanion) {
+    if (guest.seat) continue;
+    const seat = seats.at(0);
+    if (!seat) break;
+
+    guest.seat = seat;
+    guestToSeat.set(guest, seat);
+    seats = seats.filter(({ id }) => id !== seat.id);
+  }
+
+  const guestsWithLocationSeat = getGuestsWithLocationSeat(
+    guests,
+    seats
+  ) as GuestWithLocationSeat[];
+  const score = calculateArrangementScore(guestsWithLocationSeat);
+
+  return { guests, score };
+}
+
+type GuestWithLocationSeat = Guest & { seat: SeatWithLocation };
+
+const getGuestsWithLocationSeat = (
+  guests: Guest[],
+  seats: SeatWithLocation[]
+) => {
+  return guests.map((guest) => {
+    const seat = seats.find((s) => s.id === guest.seat?.id);
+    if (!seat) return guest;
+    return { ...guest, seat } as GuestWithLocationSeat;
+  }) as GuestWithLocationSeat[];
+};
+
+function calculateArrangementScore(guests: GuestWithLocationSeat[]) {
+  const allScores: number[] = [];
+  for (const guest of guests) {
+    const distances = getDistancesToFriends(guest, guests);
+    const meanDistance = mean(distances);
+
+    const bestTwo = distances.sort((a, b) => a - b).slice(0, 1);
+
+    if (isNaN(meanDistance)) {
+      allScores.push(0);
+      continue;
+    }
+
+    if (bestTwo.every((d) => d < 100)) {
+      allScores.push(1);
+      continue;
+    }
+    if (bestTwo.every((d) => d < 200)) {
+      allScores.push(0.5);
+      continue;
+    }
+    if (bestTwo.every((d) => d < 300)) {
+      allScores.push(0.25);
+      continue;
+    }
+  }
+
+  const replacedScores = allScores.map((s) => (isNaN(s) ? 0 : s));
+  const score = mean(replacedScores) ?? 0;
+  // console.log('SCORE: ', score);
+  // if (score == 1) console.log('replaced', replacedScores);
+  return isNaN(score) ? 0 : score;
+}
+
+function mean(numbers: number[]) {
+  return numbers.reduce((a, b) => a + b, 0) / numbers.length;
+}
+
+function getAllFriends(
+  guest: GuestWithLocationSeat,
+  guests: GuestWithLocationSeat[]
+) {
+  const ownFriends = guests.filter((g) => guest.friendNames.includes(g.name));
+  const otherFriends = guests.filter((g) => g.friendNames.includes(guest.name));
+  const setList = new Set([...ownFriends, ...otherFriends]).values();
+  return Array.from(setList);
+}
+
+function getDistancesToFriends(
+  guest: GuestWithLocationSeat,
+  guests: GuestWithLocationSeat[]
+): number[] {
+  const friends = getAllFriends(guest, guests);
+  const distances = [];
+
+  for (const friend of friends) {
+    const distance = getDistanceBetweenSeats(guest.seat, friend.seat);
+    distances.push(distance);
+  }
+
+  return distances;
+}
+
+function getDistanceBetweenSeats(
+  seat1: SeatWithLocation,
+  seat2: SeatWithLocation
+) {
+  const x = seat1.x - seat2.x;
+  const y = seat1.y - seat2.y;
+  return Math.sqrt(x * x + y * y);
+}
