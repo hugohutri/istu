1d38dc5f4ec91f626e5d157d8500ad7293ee430f
hugohutri <55588133+hugohutri@users.noreply.github.com>
2023-01-16 21:46:52 +0200

    feat: fix missing code

--- src/components/uikit/Dropdown.tsx
+++ src/components/uikit/Dropdown.tsx
@@ -1,31 +1,20 @@
 import { useState } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { AnimatedChevron } from '../AnimatedChevron';
 import { Body } from './Body';
 
 const DropdownContainer = styled.div`
   position: relative;
+  cursor: pointer;
 `;
 
-const DropdownButton = styled(Body)<{ disabled: boolean }>`
+const DropdownButton = styled(Body)`
+  background-color: #fff;
+  border: 1px solid ${(props) => props.theme.color.text};
   padding: 0.3rem 0.5rem;
   display: flex;
   justify-content: space-between;
   align-items: center;
-
-  background-color: ${(props) => props.theme.color.background};
-  border: 1px solid ${(props) => props.theme.color.text};
-  color: ${(props) => props.theme.color.text};
-  cursor: pointer;
-
-  ${(props) =>
-    props.disabled &&
-    css`
-      color: ${props.theme.color.muted1};
-      background-color: ${props.theme.color.muted5};
-      border: 1px solid ${props.theme.color.muted2};
-      cursor: auto;
-    `};
 `;
 
 const DropdownList = styled.ul`
@@ -51,7 +40,6 @@ const DropdownItem = styled(Body)`
 type DropdownProps = {
   options: string[];
   onChange: (option: string) => void;
-  disabled?: boolean;
   defaultOption?: string;
 };
 
@@ -59,7 +47,6 @@ export const Dropdown = ({
   options,
   defaultOption,
   onChange,
-  disabled = false,
 }: DropdownProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const [selectedOption, setSelectedOption] = useState(
@@ -78,12 +65,7 @@ export const Dropdown = ({
 
   return (
     <DropdownContainer>
-      <DropdownButton
-        disabled={disabled}
-        size="small"
-        variant="bold"
-        onClick={toggleDropdown}
-      >
+      <DropdownButton size="small" variant="bold" onClick={toggleDropdown}>
         {selectedOption}
         <AnimatedChevron open={isOpen} size="0.7rem" />
       </DropdownButton>
