897ab98ae208df4b2859ffd15c2b4869790403ce
hugohutri <55588133+hugohutri@users.noreply.github.com>
2023-01-16 22:17:02 +0200

    fix: git problems

--- src/hooks/useGuests.tsx
+++ src/hooks/useGuests.tsx
@@ -1,191 +1,177 @@
-import { Guest, Seat } from './types'
-import create from 'zustand'
-import { persist } from 'zustand/middleware'
+import { Guest, Seat } from './types';
+import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 type RelatedGuests = {
-    friends: Guest[]
-    companion: Guest | undefined
-    self: Guest
-    others: Guest[]
-}
+  friends: Guest[];
+  companion: Guest | undefined;
+  self: Guest;
+  others: Guest[];
+};
 
 type GuestsStore = {
-    guests: Guest[]
-    setGuests: (guests: Guest[]) => void
-    addGuest: (newGuest: Guest) => void
-    addGuests: (newGuests: Guest[]) => void
-    editGuest: (oldGuest: Guest, newGuest: Guest) => void
-    assignSeat: (seat: Seat, guest: Guest) => void
-    removeGuest: (guest: Guest) => void
-    getRelatedGuests: (guest: Guest) => RelatedGuests
-}
+  guests: Guest[];
+  setGuests: (guests: Guest[]) => void;
+  addGuest: (newGuest: Guest) => void;
+  addGuests: (newGuests: Guest[]) => void;
+  editGuest: (oldGuest: Guest, newGuest: Guest) => void;
+  assignSeat: (seat: Seat, guest: Guest) => void;
+  removeGuest: (guest: Guest) => void;
+  getRelatedGuests: (guest: Guest) => RelatedGuests;
+};
 
 export const useGuests = create<GuestsStore>()(
-    persist(
-        (set, get) => ({
-            guests: [],
-            setGuests: (guests) => set(() => ({ guests: [...guests] })),
-            addGuest: (newGuest) =>
-                set(({ guests }) => {
-                    // if guest with same name excists, add (index) to the end of the name. eg. John Doe (2)
-                    if (isNameTaken(guests, newGuest)) {
-                        const updatedGuest = getUserWithIndexedName(
-                            guests,
-                            newGuest
-                        )
-                        return { guests: [...guests, updatedGuest] }
-                    }
+  persist(
+    (set, get) => ({
+      guests: [],
+      setGuests: (guests) => set(() => ({ guests: [...guests] })),
+      addGuest: (newGuest) =>
+        set(({ guests }) => {
+          // if guest with same name excists, add (index) to the end of the name. eg. John Doe (2)
+          if (isNameTaken(guests, newGuest)) {
+            const updatedGuest = getUserWithIndexedName(guests, newGuest);
+            return { guests: [...guests, updatedGuest] };
+          }
 
-                    return { guests: [...guests, newGuest] }
-                }),
-
-            addGuests: (newGuests) =>
-                set(({ guests }) => {
-                    const updatedGuests = [...guests]
-
-                    for (const guest of newGuests) {
-                        if (isNameTaken(updatedGuests, guest)) {
-                            const updatedGuest = getUserWithIndexedName(
-                                updatedGuests,
-                                guest
-                            )
-                            updatedGuests.push(updatedGuest)
-                        } else {
-                            updatedGuests.push(guest)
-                        }
-                    }
-
-                    return { guests: updatedGuests }
-                }),
-
-            editGuest: (oldGuest, newGuest) => {
-                set((state) => {
-                    const newGuests = [...state.guests]
-                    const index = newGuests.findIndex(
-                        (g) => g.name === oldGuest.name
-                    )
-                    newGuests[index] = newGuest
-                    return { guests: newGuests }
-                })
-            },
-
-            assignSeat: (seat, guest) => {
-                set((state) => {
-                    const newGuests = [...state.guests]
-
-                    // Remove seat from the guest that was previously assigned to it
-                    // Ns. Vedä tuoli alta
-                    const previousGuestIdx = newGuests.findIndex(
-                        (g) => g.seat?.id === seat.id
-                    )
-
-                    if (previousGuestIdx !== -1) {
-                        newGuests[previousGuestIdx].seat = undefined
-                    }
-
-                    // Assign seat to the new guest
-                    const index = newGuests.findIndex(
-                        (g) => g.name === guest.name
-                    )
-                    newGuests[index].seat = seat
-                    return { guests: newGuests }
-                })
-            },
-
-            removeGuest: (guest) => {
-                set((state) => {
-                    const newGuests = [...state.guests]
-                    const index = newGuests.findIndex(
-                        (g) => g.name === guest.name
-                    )
-                    newGuests.splice(index, 1)
-                    return { guests: newGuests }
-                })
-            },
-
-            getRelatedGuests: (guest) => {
-                const { guests } = get()
-                const otherFriends = guests.filter((g) =>
-                    g.friendNames.includes(guest.name)
-                )
-                const ownFriends = guests.filter((g) =>
-                    guest.friendNames.includes(g.name)
-                )
-                const friends = [...otherFriends, ...ownFriends]
-                const companion = guests.find((g) => g.name === guest.avecName)
-
-                const others = guests.filter(
-                    (g) =>
-                        g.name !== guest.name &&
-                        !friends.includes(g) &&
-                        g !== companion
-                )
-                return {
-                    friends,
-                    companion,
-                    self: guest,
-                    others,
-                }
-            },
+          return { guests: [...guests, newGuest] };
         }),
-        {
-            name: 'guest-storage',
-        }
-    )
-)
+
+      addGuests: (newGuests) =>
+        set(({ guests }) => {
+          const updatedGuests = [...guests];
+
+          for (const guest of newGuests) {
+            if (isNameTaken(updatedGuests, guest)) {
+              const updatedGuest = getUserWithIndexedName(updatedGuests, guest);
+              updatedGuests.push(updatedGuest);
+            } else {
+              updatedGuests.push(guest);
+            }
+          }
+
+          return { guests: updatedGuests };
+        }),
+
+      editGuest: (oldGuest, newGuest) => {
+        set((state) => {
+          const newGuests = [...state.guests];
+          const index = newGuests.findIndex((g) => g.name === oldGuest.name);
+          newGuests[index] = newGuest;
+          return { guests: newGuests };
+        });
+      },
+
+      assignSeat: (seat, guest) => {
+        set((state) => {
+          const newGuests = [...state.guests];
+
+          // Remove seat from the guest that was previously assigned to it
+          // Ns. Vedä tuoli alta
+          const previousGuestIdx = newGuests.findIndex(
+            (g) => g.seat?.id === seat.id
+          );
+
+          if (previousGuestIdx !== -1) {
+            newGuests[previousGuestIdx].seat = undefined;
+          }
+
+          // Assign seat to the new guest
+          const index = newGuests.findIndex((g) => g.name === guest.name);
+          newGuests[index].seat = seat;
+          return { guests: newGuests };
+        });
+      },
+
+      removeGuest: (guest) => {
+        set((state) => {
+          const newGuests = [...state.guests];
+          const index = newGuests.findIndex((g) => g.name === guest.name);
+          newGuests.splice(index, 1);
+          return { guests: newGuests };
+        });
+      },
+
+      getRelatedGuests: (guest) => {
+        const { guests } = get();
+        const otherFriends = guests.filter((g) =>
+          g.friendNames.includes(guest.name)
+        );
+        const ownFriends = guests.filter((g) =>
+          guest.friendNames.includes(g.name)
+        );
+        const friends = [...otherFriends, ...ownFriends];
+        const companion = guests.find((g) => g.name === guest.avecName);
+
+        const others = guests.filter(
+          (g) =>
+            g.name !== guest.name && !friends.includes(g) && g !== companion
+        );
+        return {
+          friends,
+          companion,
+          self: guest,
+          others,
+        };
+      },
+    }),
+    {
+      name: 'guest-storage',
+    }
+  )
+);
 
 export const getInitials = (guest: Guest, limit: number) => {
-    return guest.name
-        .split(' ')
-        .map((n) => n[0])
-        .slice(0, limit)
-        .join('')
-        .replace('(', '')
-}
+  return guest.name
+    .split(' ')
+    .map((n) => n[0])
+    .slice(0, limit)
+    .join('')
+    .replace('(', '');
+};
 
 // Name guests with same name like this:
 // John Doe, John Doe (2), John Doe (3), John Doe (4)
 const getNextAvailableName = (guests: Guest[], guest: Guest) => {
-    const idx = getNextAvailableIndex(guests, guest)
-    console.log({ idx })
-    return idx > 1 ? `${guest.name} (${idx})` : guest.name
-}
+  const idx = getNextAvailableIndex(guests, guest);
+  console.log({ idx });
+  return idx > 1 ? `${guest.name} (${idx})` : guest.name;
+};
 
 function getNextAvailableIndex(guests: Guest[], guest: Guest) {
-    // Create a regular expression that matches the guest name without the index in parenthesis
-    const nameRegex = new RegExp(`^${guest.name.replace(' (\\d+)', '')}`)
+  // Create a regular expression that matches the guest name without the index in parenthesis
+  const nameRegex = new RegExp(`^${guest.name.replace(' (\\d+)', '')}`);
 
-    // Find all guests that match the regular expression
-    const matchingGuests = guests.filter((g) => nameRegex.test(g.name))
+  // Find all guests that match the regular expression
+  const matchingGuests = guests.filter((g) => nameRegex.test(g.name));
 
-    // Find the maximum index of all matching guests
-    let maxIndex = 0
-    for (const g of matchingGuests) {
-        const match = g.name.match(/\((\d+)\)$/)
-        if (match) {
-            const index = parseInt(match[1], 10)
-            if (index > maxIndex) {
-                maxIndex = index
-            }
-        }
+  // Find the maximum index of all matching guests
+  let maxIndex = 0;
+  for (const g of matchingGuests) {
+    const match = g.name.match(/\((\d+)\)$/);
+    if (match) {
+      const index = parseInt(match[1], 10);
+      if (index > maxIndex) {
+        maxIndex = index;
+      }
     }
+  }
 
-    if (maxIndex === 0) {
-        return 2
-    }
+  if (maxIndex === 0) {
+    return 2;
+  }
 
-    // Return the next available index
-    return maxIndex + 1
+  // Return the next available index
+  return maxIndex + 1;
 }
 
 const getUserWithIndexedName = (guests: Guest[], guest: Guest) => {
-    const name = getNextAvailableName(guests, guest)
-    return { ...guest, name }
-}
+  const name = getNextAvailableName(guests, guest);
+  return { ...guest, name };
+};
 
 const isNameTaken = (guests: Guest[], guest: Guest) => {
-    return guests.some((g) => g.name === guest.name)
-}
+  return guests.some((g) => g.name === guest.name);
+};
 
 // function getNextAvailableIndex(guests: Guest[], guest: Guest) {
 //   // Regular expression that matches the name and index in parenthesis
--- src/modals/AddTableModal.tsx
+++ src/modals/AddTableModal.tsx
@@ -1,120 +1,116 @@
-import { useState } from 'react'
-import styled from 'styled-components'
-import { Modal } from '../components/uikit/Modal'
+import { useState } from 'react';
+import styled from 'styled-components';
+import { Modal } from '../components/uikit/Modal';
 // import { Seats, Side, Table } from '../hooks/types';
-import { createTableObject, useTables } from '../hooks/useTables'
-import { Button } from '../components/uikit/Button'
-import { Body } from '../components/uikit/Body'
-import { Input } from '../components/uikit/Input'
-import { Spacer } from '../components/uikit/Spacer'
+import { createTableObject, useTables } from '../hooks/useTables';
+import { Button } from '../components/uikit/Button';
+import { Body } from '../components/uikit/Body';
+import { Input } from '../components/uikit/Input';
+import { Spacer } from '../components/uikit/Spacer';
 
 const SeatForm = styled.form`
-    display: flex;
-    flex-direction: column;
-`
+  display: flex;
+  flex-direction: column;
+`;
 
 export const AddTableButton = () => {
-    const [isOpen, setIsOpen] = useState(false)
-    const [topSeats, setTopSeats] = useState(0)
-    const [bottomSeats, setBottomSeats] = useState(0)
-    const [leftSeats, setLeftSeats] = useState(0)
-    const [rightSeats, setRightSeats] = useState(0)
-    const [tableName, setTableName] = useState('')
-    const addTable = useTables((store) => store.addTable)
+  const [isOpen, setIsOpen] = useState(false);
+  const [topSeats, setTopSeats] = useState(0);
+  const [bottomSeats, setBottomSeats] = useState(0);
+  const [leftSeats, setLeftSeats] = useState(0);
+  const [rightSeats, setRightSeats] = useState(0);
+  const [tableName, setTableName] = useState('');
+  const addTable = useTables((store) => store.addTable);
 
-    const resetTableValues = () => {
-        setBottomSeats(0)
-        setLeftSeats(0)
-        setRightSeats(0)
-        setTopSeats(0)
-        setTableName('')
-    }
+  const resetTableValues = () => {
+    setBottomSeats(0);
+    setLeftSeats(0);
+    setRightSeats(0);
+    setTopSeats(0);
+    setTableName('');
+  };
 
-    const onClickCreateTable = () => {
-        const table = createTableObject({
-            seatCount: {
-                top: topSeats,
-                right: rightSeats,
-                bottom: bottomSeats,
-                left: leftSeats,
-            },
-            tableName,
-        })
-        console.log('table', table)
-        addTable(table)
-        setIsOpen(false)
-        resetTableValues()
-    }
+  const onClickCreateTable = () => {
+    const table = createTableObject({
+      seatCount: {
+        top: topSeats,
+        right: rightSeats,
+        bottom: bottomSeats,
+        left: leftSeats,
+      },
+      tableName,
+    });
+    console.log('table', table);
+    addTable(table);
+    setIsOpen(false);
+    resetTableValues();
+  };
 
-    return (
-        <>
-            <Button variant="neutral" onClick={() => setIsOpen(true)}>
-                Add table
-            </Button>
-            <Modal
-                title="Add table"
-                isOpen={isOpen}
-                onClose={() => setIsOpen(false)}
-            >
-                <SeatForm>
-                    <Input
-                        label="Name of table"
-                        placeholder="Name of the table here..."
-                        type="string"
-                        name="tableName"
-                        value={tableName}
-                        onChange={(e) => setTableName(e.target.value)}
-                    />
-                    <Spacer amount="50px" />
+  return (
+    <>
+      <Button variant="neutral" onClick={() => setIsOpen(true)}>
+        Add table
+      </Button>
+      <Modal title="Add table" isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        <SeatForm>
+          <Input
+            label="Name of table"
+            placeholder="Name of the table here..."
+            type="string"
+            name="tableName"
+            value={tableName}
+            onChange={(e) => setTableName(e.target.value)}
+          />
+          <Spacer amount="50px" />
 
-                    <Body variant="bold">Number of seats on each side:</Body>
+          <Body variant="bold">Number of seats on each side:</Body>
 
-                    <Input
-                        label="Seats on top"
-                        placeholder="top"
-                        name="top"
-                        type="number"
-                        value={topSeats}
-                        onChange={(e) => setTopSeats(+e.target.value)}
-                    />
+          <Input
+            label="Seats on top"
+            placeholder="top"
+            name="top"
+            type="number"
+            value={topSeats}
+            onChange={(e) => setTopSeats(+e.target.value)}
+          />
 
-                    <Input
-                        label="Seats on right"
-                        placeholder="right"
-                        name="right"
-                        type="number"
-                        value={rightSeats}
-                        onChange={(e) => setRightSeats(+e.target.value)}
-                    />
+          <Input
+            label="Seats on right"
+            placeholder="right"
+            name="right"
+            type="number"
+            value={rightSeats}
+            onChange={(e) => setRightSeats(+e.target.value)}
+          />
 
-                    <Input
-                        label="Seats on bottom"
-                        placeholder="bottom"
-                        name="bottom"
-                        type="number"
-                        value={bottomSeats}
-                        onChange={(e) => setBottomSeats(+e.target.value)}
-                    />
+          <Input
+            label="Seats on bottom"
+            placeholder="bottom"
+            name="bottom"
+            type="number"
+            value={bottomSeats}
+            onChange={(e) => setBottomSeats(+e.target.value)}
+          />
 
-                    <Input
-                        label="Seats on left"
-                        placeholder="left"
-                        name="left"
-                        type="number"
-                        value={leftSeats}
-                        onChange={(e) => setLeftSeats(+e.target.value)}
-                    />
+          <Input
+            label="Seats on left"
+            placeholder="left"
+            name="left"
+            type="number"
+            value={leftSeats}
+            onChange={(e) => setLeftSeats(+e.target.value)}
+          />
 
-                    <Spacer amount="50px" />
-                    <Button
-                        variant="primary"
-                        disabled={tableName.length === 0}
-                        onClick={onClickCreateTable}
-                    >
-                        Create Table
-                    </Button>
-                </SeatForm>
-            </Modal>
-        </>
-    )
-}
+          <Spacer amount="50px" />
+          <Button
+            variant="primary"
+            disabled={tableName.length === 0}
+            onClick={onClickCreateTable}
+          >
+            Create Table
+          </Button>
+        </SeatForm>
+      </Modal>
+    </>
+  );
+};
